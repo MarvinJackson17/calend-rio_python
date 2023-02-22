b1cdda1912ce41fa7182cd9d9aacc99b2e58f0c3
Marvin Jackson <90123061+MarvinJackson17@users.noreply.github.com>
2023-02-22 18:00:28 -0300

    Add files via upload

---
+++ calendario.py
@@ -0,0 +1,10 @@
+# script rápido e fácil em Python para gerar um calendario
+
+# importar modulo
+import calendar
+
+# Se precisar mudar o ano basta mudar na linha 7
+yy = 2023
+
+# Mostra o calendario
+print(calendar.calendar(yy))
